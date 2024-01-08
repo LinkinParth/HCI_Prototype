4452a80d0a025799c84261c009378a71cd0779e2
GitJava1 <paul_sass@online.de>
2024-01-08 19:47:52 +0100

    profile

--- scriptforcreate.js
+++ scriptforcreate.js
@@ -1,2 +1,27 @@
-var username = prompt("Bitte gib deinen Namen ein:");
-document.getElementById("username").textContent = username;
+function setUsername() {
+    var username = document.getElementById("benutzer-input").value; 
+    document.getElementById("username").textContent = username;
+
+    var allergien = getSelectedOptions("allergien-select");
+    var preferenzen = getSelectedOptions("preferenzen-select");
+
+    console.log("Ausgewählte Allergien:", allergien);
+    console.log("Ausgewählte Präferenzen:", preferenzen);
+}
+
+function getSelectedOptions(selectId) {
+    var selectElement = document.getElementById(selectId);
+    var selectoptions = [];
+    for (let i = 0; i < selectElement.options.length; i++) {
+        if (selectElement.options[i].selected) {
+            selectoptions.push(selectElement.options[i].value);
+        }
+        
+    }
+    return selectoptions;
+}
+
+document.getElementById("profile-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+});
+
