c1d72716fab45faa2782dab55152b3e65d457bb3
GitJava1 <paul_sass@online.de>
2024-01-08 21:27:44 +0100

    Edit and Create

--- scriptforcreate.js
+++ scriptforcreate.js
@@ -1,12 +1,39 @@
 function setUsername() {
-    var username = document.getElementById("benutzer-input").value; 
+    var username = document.getElementById("username-input").value; 
     document.getElementById("username").textContent = username;
 
     var allergien = getSelectedOptions("allergien-select");
     var preferenzen = getSelectedOptions("preferenzen-select");
+    var bestätigung = confirm("Möchtest du das Profil erstellen?");
 
     console.log("Ausgewählte Allergien:", allergien);
     console.log("Ausgewählte Präferenzen:", preferenzen);
+
+    if (bestätigung) {
+        window.location.href = 'menu.html';
+    }else {
+        console.log("Profil nicht regestriert!");
+    }
+
+    
+}
+
+function speichern() {
+    var username = document.getElementById("username-input").value; 
+    document.getElementById("username").textContent = username;
+
+    var allergien = getSelectedOptions("allergien-select");
+    var preferenzen = getSelectedOptions("preferenzen-select");
+    var bestätigung = confirm("Möchtest du das Profil speichern?");
+
+    console.log("Ausgewählte Allergien:", allergien);
+    console.log("Ausgewählte Präferenzen:", preferenzen);
+
+    if (bestätigung) {
+        window.location.href = 'menu.html';
+    }else {
+        console.log("Profil nicht gespeichert!");
+    }
 }
 
 function getSelectedOptions(selectId) {
